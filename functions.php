bdfc13016aad84a55ddf4cb963526b868b4a80d9
Dan Tovbein <dtovbein@greenpeace.org>
2024-11-20 15:01:00 -0300

    PLANET-7666: Implement subscription form on footer (#2)
    * Implement subscription form on footer
    
    Ref: https://jira.greenpeace.org/browse/PLANET-7666
    
    - Apply new background styles to signup form
    - Apply new styles to thankyou message
    
    * Move EveryAction external resources into functions
    
    - Reduce duplicate the same library on pages every time the page is loaded
    
    - This will reduce to fetch the same resource everytime they have
      at least 2 forms into a page.

--- functions.php
+++ functions.php
@@ -12,4 +12,14 @@ function enqueue_child_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
 
+/**
+ * Include all needed scripts to be used by EveryAction signup & contact information forms
+ */
+add_action( 'wp_enqueue_scripts', 'enqueue_every_action_form_scripts', 99);
+
+function enqueue_every_action_form_scripts() {
+	wp_enqueue_script('everyaction-js','https://static.everyaction.com/ea-actiontag/at.js',);
+	wp_enqueue_style( 'everyaction-styles', 'https://static.everyaction.com/ea-actiontag/at.min.css' );
+}
+
 require_once 'wp_all_import_functions.php';
